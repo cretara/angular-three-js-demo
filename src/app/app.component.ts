d976679a77c671f4d5456576f71740b097b4f0e8
Pier Daniele Cretara <pierdaniele.cretara@dxc.com>
2024-09-12 12:32:33 +0200

    Created a cube in Angular component app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import * as THREE from "three";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,48 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-three-js-demo';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') private canvasRef!: ElementRef<HTMLCanvasElement>;
+
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private cube!: THREE.Mesh;
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.initThreeJS();
+    this.animate();
+  }
+
+  private initThreeJS(): void {
+    // Renderer
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvasRef.nativeElement});
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Scene
+    this.scene = new THREE.Scene();
+
+    // Camera
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 5;
+
+    // Cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+  }
+
+  private animate(): void {
+    requestAnimationFrame(() => this.animate());
+
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
 }
