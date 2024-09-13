812e71387875b3603356cac6ed0d4bf5e6fd413f
Pier Daniele Cretara <pierdaniele.cretara@dxc.com>
2024-09-13 15:28:58 +0200

    Added rotations and objects transformations; commented camera instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import * as THREE from "three";
+import {Clock} from "three";
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,8 @@ export class AppComponent implements AfterViewInit {
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private cube!: THREE.Mesh;
+  private clock: THREE.Clock = new Clock();
+  private delta = 0;
 
   constructor() {
   }
@@ -44,21 +47,34 @@ export class AppComponent implements AfterViewInit {
     // Camera
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.z = 5;
+    // this.camera.rotation.y = Math.PI / 2;
 
     // Cube
-    const geometry = new THREE.BoxGeometry(1,1, 2);
-    const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    const geometry = new THREE.BoxGeometry(1, 1, 2);
+    const material = new THREE.MeshBasicMaterial();
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
+
+    //Axis Helper
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
   }
 
   /**
    * Animate the cube and render the scene
    */
   private animate(): void {
-    requestAnimationFrame(() => this.animate());
-    this.cube.rotation.x += 0.02;
-    this.cube.rotation.y += 0.02;
+    this.delta = this.clock.getDelta();
+    const elapsedTime = this.clock.getElapsedTime();
+    requestAnimationFrame(this.animate.bind(this));
+    // this.cube.position.x = Math.cos(this.delta);
+    this.cube.position.x = Math.cos(elapsedTime);
+    this.cube.position.y = Math.sin(elapsedTime);
+    // this.camera.rotation.y = this.cube.rotation.y;
+    // this.camera.rotation.x = this.cube.rotation.x;
+    // this.camera.lookAt(this.cube.position);
+    // this.camera.updateProjectionMatrix();
+    // console.log("this.cube.quaternion", this.cube.quaternion);
     this.renderer.render(this.scene, this.camera);
   }
 
